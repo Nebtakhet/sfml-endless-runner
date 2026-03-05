7a331ea954a0d127638586052b8d308cad876368
César <cesasanc@student.hive.fi>
2026-03-05 16:44:33 +0200

    Updated enemy

--- src/entities/Enemy.cpp
+++ src/entities/Enemy.cpp
@@ -16,7 +16,7 @@ bool Enemy::init()
 	sf::FloatRect localBounds = m_pSprite->getLocalBounds();
 	m_pSprite->setOrigin(sf::Vector2f(localBounds.size.x / 2.0f, localBounds.size.y / 2.0f));
 	m_pSprite->setPosition(m_position);
-	m_pSprite->setScale(sf::Vector2f(2.5f, 2.5f));
+	m_pSprite->setScale(sf::Vector2f(0.1f, 0.1f));
 	m_collisionRadius = collisionRadius;
 
 	setHealth(3);
